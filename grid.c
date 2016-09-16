85c213593aa50a6451b4853f66ebe210dc2c70a7
Drew Mark Masters <dmaster1@hydra1.eecs.utk.edu>
2016-09-16 14:22:02 -0400

    wrote initial code for project

--- grid.c
+++ grid.c
@@ -8,7 +8,20 @@ int main(int argc, char **argv){
 		exit(1);
 	}
 	
-	int dim;
+	int dim, world_size, world_rank;;
+	/*read in grid dimension specified by user*/
 	dim = atoi(argv[1]);
 	
+	/*initialize MPI*/
+	MPI_Init(NULL,NULL);
+
+	/*get world size and rank*/
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	
+	/*create communicator and groups*/
+	
+	/*pass token*/
+
+	MPI_Finalize();
 }
