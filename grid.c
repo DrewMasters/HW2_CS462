af26b329099bcd324900f345a24e8bd6d1ddde69
Drew Mark Masters <dmaster1@hydra1.eecs.utk.edu>
2016-09-18 17:03:56 -0400

    makes correct groups for n=2 and n=3

--- grid.c
+++ grid.c
@@ -22,13 +22,23 @@ int main(int argc, char **argv){
 	max_color = 2*n-2;
 
 	/*create communicator and groups*/
-	if (world_rank == n-1) color = max_color;
-	else if (world_rank == (n*n)-1-(n-1)) color = max_color -1;
+	if (world_rank == n-1){
+		color = max_color;
+		printf("color %d world rank %d world size %d if\n", color,world_rank, world_size);
+	}
+	else if (world_rank == (n*n)-1-(n-1)){
+		if (n!=3) color = max_color -1;
+		else color = 2;
+		printf("color %d world rank %d world size %d (n*n)-n-2 %d\n", color,world_rank, world_size,(n*n)-n-2);
+	}
 	else if (n==5){/*need to fix this can only handle n=4 and n=5*/
 		if (world_rank==15||world_rank==21) color = max_color -2;
 		else color=world_rank %(n+1);
 	}
-	else color = world_rank % (n+1);
+	else {
+		color = world_rank % (n+1);
+		printf("color %d world rank %d world size %d else\n", color,world_rank, world_size);
+	}
 	/*printf("color %d world rank %d world size %d\n", color, world_rank, world_size);
 	*/
 	MPI_Comm diag;
