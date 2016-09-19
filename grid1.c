d3b7cd245085ed8a57deb6c948dbf7ad53685fa3
Drew Masters <drewmasters78@gmail.com>
2016-09-19 13:51:27 -0400

    message passing code added

--- grid1.c
+++ grid1.c
@@ -18,18 +18,21 @@ int main(int argc, char **argv){
 		exit(1);
 	}
 
-	int row,max_color,color;
+	int row,max_color,color,flag;
 	max_color = 2*n-2;
+	flag=0;
 
 	/*create communicator and groups*/
 	if (world_rank == n-1){
 		/*gets upper right corner of grid*/
 		color = max_color;
+		flag=1;
 	}
 	else if (world_rank == (n*n)-1-(n-1)){
 		/*gets lower left corner of grid*/
 		if (n!=3) color = max_color -1;
 		else color = 2;
+		flag=1;
 	}
 	else if ((world_rank % (n+1))>=3 && (world_rank % (n+1))<(n-1)){
 		row = world_rank/n;
@@ -56,12 +59,38 @@ int main(int argc, char **argv){
 	MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &diag);
 	int diag_rank, diag_size;
 	MPI_Comm_rank(diag, &diag_rank);
-	MPI_Comm_rank(diag, &diag_size);
+	MPI_Comm_size(diag, &diag_size);
 	
-	printf("diag rank %d world rank %d color %d\n", diag_rank, world_rank, color);
-	 
+	printf("diag rank %d/%d world rank %d color %d\n", diag_rank, diag_size,world_rank, color);
+	
+	int token;
 
-	/*pass token*/
-
+	if (diag_rank == 0 && flag != 1){
+		token = world_rank;
+		MPI_Send(&token, 1, MPI_INT, diag_rank+1, 0, diag);
+		printf("%d sent token\n",world_rank);
+		MPI_Recv(&token, 1, MPI_INT, diag_rank+1, 0, diag, MPI_STATUS_IGNORE);	
+		printf("token back at starting point\n");
+	}
+	else if (flag != 1){
+		MPI_Recv(&token, 1, MPI_INT, diag_rank-1, 0, diag, MPI_STATUS_IGNORE);
+		printf("%d received token from %d\n",world_rank, token);
+		if (diag_rank+1 != diag_size){
+			token=world_rank;
+			MPI_Send(&token, 1, MPI_INT, diag_rank+1, 0, diag);
+			printf("%d sent token\n",world_rank);
+			MPI_Recv(&token, 1, MPI_INT, diag_rank+1, 0, diag, MPI_STATUS_IGNORE);
+			printf("%d received token from %d\n",world_rank,token);
+		}
+		MPI_Send(&token, 1, MPI_INT, diag_rank-1, 0, diag);
+		printf("%d sent token back up diagonal\n", world_rank);
+	}
+	else{
+		MPI_Send(&token, 1, MPI_INT, diag_rank, 0, diag);
+		printf("sent token to self %d\n",world_rank);
+		MPI_Recv(&token, 1, MPI_INT, diag_rank, 0, diag, MPI_STATUS_IGNORE);
+		printf("received token from self %d\n",world_rank);
+	}
+	
 	MPI_Finalize();
 }
